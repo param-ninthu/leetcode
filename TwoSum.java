4e481075f89bf9f6e4c95268778da7376f0c7ee7
param-ninthu <67898034+param-ninthu@users.noreply.github.com>
2023-02-11 22:27:30 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,18 @@
+// Exceed time limit
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int[] outArray = new int[2];
+
+//         for(int i =0 ; i < numbers.length ; i++){
+//             for(int j = i+1 ; j < numbers.length ; j++){
+//                 if(numbers[i]+numbers[j] == target){
+//                     outArray[0]=i+1;
+//                     outArray[1]= j+1;
+//                     return outArray;
+//                 }
+//             }
+//         }
+//         return outArray;
+//     }
+// }
+
