170d2971681135c55961ac75a94d302bc0aa69f2
param-ninthu <67898034+param-ninthu@users.noreply.github.com>
2023-02-12 00:15:06 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -16,3 +16,34 @@
 //     }
 // }
 
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] outArray = new int[2];
+
+        if(numbers.length == 0 || numbers.length < 2){
+            return outArray;
+        }
+
+        int left = 0; 
+        int right = numbers.length - 1;
+
+
+        while(left < right){
+
+            if( numbers[left] + numbers[right] == target){
+                outArray[0] = left+1;
+                outArray[1] = right+1;
+                break;
+            }
+
+            else if(numbers[left] + numbers[right] > target){
+                right --;
+            }
+            else{
+                left ++;
+            }
+
+        }
+        return outArray;
+    }
+}
